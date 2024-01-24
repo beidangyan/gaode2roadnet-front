41b09067b26457ce3fcbb9799e67013ee81c4b17
beidangyan <zhangshuyuanzhi@163.com>
2024-01-24 15:15:19 +0800

    Window密钥通过接口定义

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Gaode from '@/components/static/config'
+
+window._AMapSecurityConfig = Gaode.securityJsCode;
 
 createApp(App).mount('#app')
